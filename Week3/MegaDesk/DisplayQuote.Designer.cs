6cb53270803c3ec2c486d8afdb7f9d5c2cb42915
Costigan-Stephen <stephenjcostigan@hotmail.com>
2020-01-25 18:04:31 -0600

    Add files via upload

---
+++ Week3/MegaDesk/DisplayQuote.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace MegaDesk
+{
+    partial class DisplayQuote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NavMainMenu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NavMainMenu
+            // 
+            this.NavMainMenu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.NavMainMenu.Location = new System.Drawing.Point(556, 404);
+            this.NavMainMenu.Name = "NavMainMenu";
+            this.NavMainMenu.Size = new System.Drawing.Size(232, 34);
+            this.NavMainMenu.TabIndex = 2;
+            this.NavMainMenu.Text = "Close";
+            this.NavMainMenu.UseVisualStyleBackColor = true;
+            this.NavMainMenu.Click += new System.EventHandler(this.NavMainMenu_Click);
+            // 
+            // DisplayQuote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.NavMainMenu;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.NavMainMenu);
+            this.Name = "DisplayQuote";
+            this.Text = "DisplayQuote";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NavMainMenu;
+    }
+}
